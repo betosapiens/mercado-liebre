ce9003852e72bdce4c15bee086eb2a62935bbe8d
betosapiens <betosapiens@gmail.com>
2022-10-14 10:48:46 -0300

    corrección de package.json (Start app.js)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(process.env.PORT || 5000, ()=>{
+app.listen(process.env.PORT || 3030, ()=>{
     console.log('Servidor funcionando');
 });
 
